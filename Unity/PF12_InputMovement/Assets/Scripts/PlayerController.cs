d4be359c96adb169480010cd8ea94da1911b8257
BakaRecca <eddie.norberg@gmail.com>
2020-10-14 13:50:38 +0200

    Update PlayerController.cs

--- Unity/PF12_InputMovement/Assets/Scripts/PlayerController.cs
+++ Unity/PF12_InputMovement/Assets/Scripts/PlayerController.cs
@@ -14,12 +14,61 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float speed = 60f;
 
+    private Vector2 input;
+    private Vector2 velocity;
+
     private void Awake ()
     {
-        
+        rBody = GetComponent<Rigidbody2D>();
+        boxCol = GetComponent<BoxCollider2D>();
     }
 
-    enum ControllerType
+    private void Update()
+    {
+        GetInput();
+    }
+    
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void GetInput()
+    {
+        input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    private void Move()
+    {
+        if (controllerType == ControllerType.Translate)
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        bool grounded = false;
+        
+        boxCol.enabled = false;
+
+        if (Physics2D.OverlapCircle(transform.position, 2f))
+            grounded = true;
+
+        boxCol.enabled = true;
+
+        return grounded;
+    }
+
+    private enum ControllerType
     {
         Translate,
         AddForce,
